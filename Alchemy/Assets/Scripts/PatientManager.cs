685d7917e7a285830735a5e7cc3e6e7668ec6ae1
IanWark <wark@ualberta.ca>
2020-02-01 13:49:50 -0500

    Add PatientManager class

---
+++ Alchemy/Assets/Scripts/PatientManager.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PatientManager : MonoBehaviour
+{
+    public Action OnPatientReset;
+
+    private eSymptom symptom = eSymptom.sympA;
+    public eSymptom Symptom { get; }
+
+    private int numberOfSymptomsPossible = Enum.GetValues(typeof(eSymptom)).Length;
+
+    private void Start()
+    {
+        ResetPatient();
+    }
+
+    void ResetPatient()
+    {
+        // Randomly generate a symptom
+        symptom = (eSymptom) UnityEngine.Random.Range(0, numberOfSymptomsPossible);
+
+        // Send event to UI
+        OnPatientReset?.Invoke();
+    }
+}
