b7d3e524da7973c6757b3b750df659664931e26c
Jeffrey Chu Chien Lin <jeffreychuchienlin@gamil.com>
2020-02-01 11:57:12 -0500

    Added basic framework for ingredient related code.

---
+++ Alchemy/Assets/Scripts/IngredientScripts/Enums.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum Ingredient
+{
+	ingreA_PLACEHOLDER,
+	ingreB_PLACEHOLDER,
+	ingreC_PLACEHOLDER,
+}
+
+public enum Symptom
+{
+	sympA_PLACEHOLDER = 0,
+	sympB_PLACEHOLDER = 1,
+	sympC_PLACEHOLDER = 2,
+}
---
+++ Alchemy/Assets/Scripts/IngredientScripts/IngredientUtils.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+List<List<int>> SymptomSets =
+{
+	{ 1, -1, -1},
+	{-1,  0,  1},
+	{ 0,  1, -1},
+};
+
+public class Ingredient
+{
+    
+}
