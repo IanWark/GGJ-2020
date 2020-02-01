87bb726f093a26f7ba497a97116aac2b748dffc4
Martin Forget <martforg@hotmail.com>
2020-02-01 17:31:08 -0500

    Added drag and drop functionality to the UI.

---
+++ Alchemy/Assets/Scripts/UI/IngredientTargetBox.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+
+public class IngredientTargetBox : MonoBehaviour, IDropHandler
+{
+    public GameUIManager gameUIManager;
+
+    public int ingredientIndex;
+
+    public void OnDrop(PointerEventData eventData)
+    {
+        if (gameUIManager.DraggedIngredient != null)
+        {
+            gameUIManager.SetIngredient(ingredientIndex, gameUIManager.DraggedIngredient);
+        }
+    }
+}
