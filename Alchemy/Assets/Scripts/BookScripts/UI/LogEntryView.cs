6f338f4b246f04f38da5a3ee3fc8f59e67fbe5e7
IanWark <wark@ualberta.ca>
2020-02-01 23:14:16 -0500

    Add LogEntryView

---
+++ Alchemy/Assets/Scripts/BookScripts/UI/LogEntryView.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class LogEntryView : MonoBehaviour
+{
+    [SerializeField]
+    public TextMeshProUGUI ingredientsText = null;
+
+    [SerializeField]
+    public TextMeshProUGUI resultsText = null;
+
+    public void SetLogEntry(string ingredientsString, string resultsString)
+    {
+        ingredientsText.text = ingredientsString;
+        resultsText.text = resultsString;
+    }
+}
