92c496e654c5747b5ea67caa03708b08f6bfccdf
Martin Forget <martforg@hotmail.com>
2020-02-02 11:56:13 -0500

    Added score UI

---
+++ Alchemy/Assets/Scripts/UI/ScoreUI.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class ScoreUI : MonoBehaviour
+{
+    [SerializeField]
+    TextMeshProUGUI scoreText;
+
+    [SerializeField]
+    TextMeshProUGUI patientsText;
+
+    void Start()
+    {
+        GameManager gm = GameManager.Instance;
+
+        patientsText.text = gm.PatientsToGo.ToString();
+        scoreText.text = "0";
+
+        gm.OnPatientsToGoChanged += OnPatientsToGoChangedListener;
+        gm.OnPatientsCuredChanged += OnPatientsCuredChangedListener;
+    }
+
+    void OnPatientsToGoChangedListener(int patientsToGo)
+    {
+        patientsText.text = patientsToGo.ToString();
+    }
+
+    void OnPatientsCuredChangedListener(int patientsCured)
+    {
+        patientsText.text = patientsCured.ToString();
+    }
+}
